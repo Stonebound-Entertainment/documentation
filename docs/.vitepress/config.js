965dbd6cde8487b23c0fc498680613379f060264
CelestialSatyr <CelestialSatyr@gmail.com>
2022-12-28 16:40:45 +0100

    Added Deploy File for GitHub Pages

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,13 +4,14 @@ export default {
   
   themeConfig: {
     siteTitle: 'Documentation',
+	base: '/documentation/',
 	nav: [
       { text: 'Documentation', link: '/documentation' },
       { text: 'Tutorials', link: '/tutorials' },
       { text: 'Release Notes', link: '/releases' }
     ],
 	editLink: {
-      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path'
+      pattern: 'https://github.com/Stonebound-Entertainment/documentation/tree/main/docs/:path'
     },
 	
 	sidebar: [
